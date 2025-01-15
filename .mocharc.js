663eb72f904fbc082d8495c8c900e1aadee28fdc
dimilidi <dimitrovalidia@yahoo.com>
2025-01-16 00:14:06 +0100

    increase test timeout

--- .mocharc.js
+++ .mocharc.js
@@ -14,7 +14,7 @@ module.exports = {
     package: './package.json',
     reporter: 'spec',
     slow: 75,
-    timeout: 9000,
+    timeout: 10000,
     ui: 'tdd',
     'watch-files': ['lib/**/*.js','test/**/*.js'],
     'watch-ignore': ['lib/vendor']
